e1cf6a78f14105c7029995b284b1439d54b421ee
ApoorvShri <lcb2021008@iiitl.ac.in>
2024-02-07 02:28:15 +0530

    Sorting and Searching - Collecting Numbers

---
+++ Sorting and Searching/Collecting_Numbers.cpp
@@ -0,0 +1,22 @@
+// Problem -> https://cses.fi/problemset/task/2217
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<int> a(n);
+  for (auto &it : a) cin >> it;
+
+  set<int> st;
+  int ans = 1;
+
+  for (auto &it : a) {
+    if (st.count(it + 1)) ans++;
+    st.insert(it);
+  }
+
+  cout << ans << endl;
+}
