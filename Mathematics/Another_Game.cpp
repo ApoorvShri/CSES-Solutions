27728021cae6b0c4ca9e3c669d1e24c4b13d5399
ApoorvShri <lcb2021008@iiitl.ac.in>
2024-04-23 19:28:22 +0530

    Game Theory 4

---
+++ Mathematics/Another_Game.cpp
@@ -0,0 +1,24 @@
+// Problem -> https://cses.fi/problemset/task/2208
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int n;
+    cin >> n;
+
+    int ans = 0;
+
+    for (int i = 0, x; i < n; i++) {
+      cin >> x;
+      ans += x % 2;
+    }
+
+    if (ans) cout << "first" << endl;
+    else cout << "second" << endl;
+  }
+}
